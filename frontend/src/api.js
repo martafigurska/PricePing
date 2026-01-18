19a0521ed3dea845b2ccd5613087d2ace9d6d49b
unknown <mfigurska@student.agh.edu.pl>
2026-01-18 20:41:16 +0100

    delete product

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,8 @@
 export const BASE_URLS = {
   addProduct: "https://add-product-b432xl5c5a-ew.a.run.app",
   getProducts: "https://get-products-b432xl5c5a-ew.a.run.app",
-  checkPrices: "https://europe-west1-priceping-480812.cloudfunctions.net/check-prices"
+  checkPrices: "https://europe-west1-priceping-480812.cloudfunctions.net/check-prices",
+  deleteProduct: "https://delete-product-b432xl5c5a-ew.a.run.app"
 };
 
 export async function addProduct(url, target_price, email) {
@@ -24,6 +25,6 @@ export async function checkPrices() {
 }
 
 export async function deleteProduct(id) {
-  const res = await fetch(`${BASE_URLS.getProducts}/delete-product?id=${id}`, { method: "DELETE" });
+  const res = await fetch(`${BASE_URLS.deleteProduct}?id=${id}`, { method: "DELETE" });
   return res.json();
 }
